3b1b39ca9e5597ffdd0e988aca0f9ca27f0d476e
Murad <muradrashidov995@gmail.com>
2023-11-23 10:17:55 +0400

    halfcomplated

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,9 @@
 const burgerMenuTrigger = document.querySelector(".burgerMenu");
 const mobileMenu = document.querySelector(".mobileMenu");
+const contact = document.querySelector(".contact");
+const aboutImage = document.querySelector(".aboutImage");
+
+
 let isOpen = false;
 
 burgerMenuTrigger.addEventListener("click",()=>{
@@ -14,3 +18,15 @@ burgerMenuTrigger.addEventListener("click",()=>{
      }
 
 })
+// aboutImage.addEventListener('mouseover',showFlex);
+// aboutImage.addEventListener('mouseleave',hideFlex);
+
+// function showFlex() {
+//    contact.classList.remove("d-none");
+//    contact.classList.add("d-flex");
+//  }
+
+//  function hideFlex() {
+//    contact.classList.remove("d-flex");
+//    contact.classList.add("d-none");
+//  }
